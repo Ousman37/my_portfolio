9e98b52e86f8f836fde1721494b650071e055b7b
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-10-13 10:52:03 +0200

    simple form validation commit!

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,3 +57,61 @@ sr.reveal(`.section-title, .section-subtitle-container`, {
 
 sr.reveal(`.portfolio-card`, { interval: 250 });
 sr.reveal(`.form-container, .footer`, { origin: `top` });
+
+// !Simple form validation
+// contact form
+const form = document.querySelector("#contactForm");
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
+const lastName = document.querySelector("#lastName");
+const lastNameError = document.querySelector("#lastNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError_contact_form");
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#messageError");
+//console.log(form);
+function validateForm3(event) {
+  event.preventDefault();
+
+  //console.log("hello");
+
+  if (checkLength(firstName.value, 0) === true) {
+    firstNameError.style.display = "none";
+  } else {
+    firstNameError.style.display = "block";
+  }
+
+  if (checkLength(lastName.value, 3) === true) {
+    lastNameError.style.display = "none";
+  } else {
+    lastNameError.style.display = "block";
+  }
+
+  if (validateEmail(email.value, 24) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  if (checkLength(message.value, 0) === true) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+}
+
+form.addEventListener("submit", validateForm3);
+
+function checkLength(value, len) {
+  if (value.length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
